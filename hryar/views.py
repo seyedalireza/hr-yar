41874cf42b55d91e41412615334f97173e21792b
Sabrineh <Sabrinehmokhtari@yahoo.com>
2020-11-14 19:57:39 +0330

    # create - list jobs added

--- hryar/views.py
+++ hryar/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.template import loader
 
 from hryar.models import Position, Company
 from django.views import generic
